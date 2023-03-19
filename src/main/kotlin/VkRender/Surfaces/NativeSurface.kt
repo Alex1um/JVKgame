2eef6e8f7bc23d29fe748d90d27b1a13ef70b424
Alex1um <assassin20563@gmail.com>
2023-03-19 13:09:52 +0700

    idea files, removed native surface

--- src/main/kotlin/VkRender/Surfaces/NativeSurface.kt
+++ src/main/kotlin/VkRender/Surfaces/NativeSurface.kt
@@ -12,19 +12,19 @@ class NativeSurface: Surface {
     override val surface: Long
     override val instance: Instance
 
-    constructor(window: Window, instance: Instance) {
-        this.instance = instance
-        with (Util) {
-            MemoryStack.stackPush().use { stack ->
-//            val lp = stack.longs(0)
-                if (GLFWVulkan.glfwCreateWindowSurface(instance.instance, window.window, null, lp) != VK13.VK_SUCCESS) {
-                    throw IllegalStateException("failed to create window surface")
-                }
-                surface = lp[0]
-            }
-        }
-    }
-
+//    constructor(window: Window, instance: Instance) {
+//        this.instance = instance
+//        with (Util) {
+//            MemoryStack.stackPush().use { stack ->
+////            val lp = stack.longs(0)
+//                if (GLFWVulkan.glfwCreateWindowSurface(instance.instance, window.window, null, lp) != VK13.VK_SUCCESS) {
+//                    throw IllegalStateException("failed to create window surface")
+//                }
+//                surface = lp[0]
+//            }
+//        }
+//    }
+//
     constructor(surface: Long, instance: Instance) {
         this.instance = instance
         this.surface = surface
